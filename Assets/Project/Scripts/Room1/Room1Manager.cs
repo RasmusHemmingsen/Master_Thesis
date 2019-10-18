9d87c4babed27efba9174711d8418fb17da9b255
rasmus hemmingsen <rasmushem93@hotmail.com>
2019-10-18 11:19:24 +0200

    added delay on disable collider on handle

--- Assets/Project/Scripts/Room1/Room1Manager.cs
+++ Assets/Project/Scripts/Room1/Room1Manager.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Room1Manager : MonoBehaviour
-{
-    
+{  
     private void Start()
     {
         Startimer();
+    }
+
+    public void DisableDoorhandle(GameObject gameObject)
+    {
+        Collider collider = gameObject.GetComponent<MeshCollider>();
+        StartCoroutine(WaitForTwoSecondsAndDisableCollider(collider));
+    }
+
+    private IEnumerator WaitForTwoSecondsAndDisableCollider(Collider collider)
+    {
+        yield return new WaitForSeconds(2);
+        collider.enabled = false;
     }
 
     public void Startimer()
