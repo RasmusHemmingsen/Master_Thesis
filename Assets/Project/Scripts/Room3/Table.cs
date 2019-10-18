436fc02e13711c6061f9eb321d2a4e9eea461e66
rasmus hemmingsen <rasmushem93@hotmail.com>
2019-10-18 18:18:33 +0200

    test for fix table

--- Assets/Project/Scripts/Room3/Table.cs
+++ Assets/Project/Scripts/Room3/Table.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class Table : MonoBehaviour
 {
     public Room3Manager m_RoomManager;
+
+    private float m_TableHight = 1f;
     private void OnTriggerEnter(Collider other)
     {
-        if (m_RoomManager.IsCurrentCube(other.gameObject))
+        if (m_RoomManager.IsCurrentCube(other.gameObject) && other.transform.position.y >= m_TableHight)
         {
             m_RoomManager.CurrentCubePlacedCorrectly();
         }
