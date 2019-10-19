68109c867a55c44fecf539ea0b98bed3c656c4ff
rasmus hemmingsen <rasmushem93@hotmail.com>
2019-10-19 19:25:04 +0200

    updated black script

--- Assets/Project/Scripts/Shared/Locomotion/PlayareaBound.cs
+++ Assets/Project/Scripts/Shared/Locomotion/PlayareaBound.cs
@@ -2,34 +2,49 @@
 
 public class PlayareaBound : MonoBehaviour
 {
-    public GameObject m_camera;
-    public GameObject m_cameraBlack;
+    public GameObject m_Camera;
+    public GameObject m_CameraBlack;
 
     public float m_PlayArea = 0.1f;
     private bool m_Isblack = false;
 
+    private GameObject m_ActiveCamera;
+
     void Start()
     {
         m_PlayArea *= m_PlayArea;
+        EnableNormalScreen();
     }
 
     void Update()
     {
-        float distance = Mathf.Pow(m_camera.transform.position.x, 2f) * Mathf.Pow(m_camera.transform.position.z, 2f);
+        float distance = Mathf.Pow(m_ActiveCamera.transform.position.x, 2f) * Mathf.Pow(m_ActiveCamera.transform.position.z, 2f);
         if(distance > m_PlayArea)
         {
             if(!m_Isblack)
             {
-                m_camera.SetActive(false);
-                m_cameraBlack.SetActive(true);
-                m_Isblack = true;
+                EnableBlackScreen();
             }
         }
         else if(m_Isblack)
         {
-            m_camera.SetActive(true);
-            m_cameraBlack.SetActive(false);
-            m_Isblack = false;
+            EnableNormalScreen();
         }
+    }
+
+    private void EnableBlackScreen()
+    {
+        m_Camera.SetActive(false);
+        m_CameraBlack.SetActive(true);
+        m_Isblack = true;
+        m_ActiveCamera = m_CameraBlack;
+    }
+
+    private void EnableNormalScreen()
+    {
+        m_Camera.SetActive(true);
+        m_CameraBlack.SetActive(false);
+        m_Isblack = false;
+        m_ActiveCamera = m_Camera;
     }
 }
