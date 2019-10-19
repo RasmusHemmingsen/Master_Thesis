9e495eeb0ce76c7325c932e76c4cd0d103a3c3a8
rasmus hemmingsen <rasmushem93@hotmail.com>
2019-10-19 19:16:19 +0200

    added new black camera to fade to black

--- Assets/Project/Scripts/Shared/Locomotion/PlayareaBound.cs
+++ Assets/Project/Scripts/Shared/Locomotion/PlayareaBound.cs
@@ -1,22 +1,18 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Valve.VR;
+﻿using UnityEngine;
 
 public class PlayareaBound : MonoBehaviour
 {
     public GameObject m_camera;
-    public float m_PlayArea = 1f;
+    public GameObject m_cameraBlack;
 
-    private float m_FadeTime = 0.5f;
+    public float m_PlayArea = 0.1f;
     private bool m_Isblack = false;
-    // Start is called before the first frame update
+
     void Start()
     {
         m_PlayArea *= m_PlayArea;
     }
 
-    // Update is called once per frame
     void Update()
     {
         float distance = Mathf.Pow(m_camera.transform.position.x, 2f) * Mathf.Pow(m_camera.transform.position.z, 2f);
@@ -24,13 +20,15 @@ public class PlayareaBound : MonoBehaviour
         {
             if(!m_Isblack)
             {
-                SteamVR_Fade.Start(Color.black, m_FadeTime, true);
+                m_camera.SetActive(false);
+                m_cameraBlack.SetActive(true);
                 m_Isblack = true;
             }
         }
         else if(m_Isblack)
         {
-            SteamVR_Fade.Start(Color.clear, m_FadeTime, true);
+            m_camera.SetActive(true);
+            m_cameraBlack.SetActive(false);
             m_Isblack = false;
         }
     }
