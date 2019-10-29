7dc649a37307cb2becd7d315cee6bbe56bfd978d
rasmus hemmingsen <rasmushem93@hotmail.com>
2019-10-29 18:56:07 +0100

    Made description for techniques

--- Assets/Project/Scripts/Room1/DescriptionManager.cs
+++ Assets/Project/Scripts/Room1/DescriptionManager.cs
@@ -38,33 +38,47 @@ public class DescriptionManager : MonoBehaviour
 
     private void SetToTeleportDescription()
     {
-        Text.text = "Teleport \n";
+        Text.text = "Teleport \n" +
+            "To use this technique point the right controller against the ground \n" +
+            "then a purple object appears and if you press on the touchpad \n" +
+            "on the right controller and you will teleport to the object";
     }
 
     private void SetToDashStepDescription()
     {
-        Text.text = "Dash Step \n";
+        Text.text = "Dash Step \n" + 
+            "To use this technique press the left touchpad \n" + 
+            "and you will dash a step in the direction you are looking";
     }
 
     private void SetToBlinkStepDescription()
     {
-        Text.text = "Blink Step \n";
+        Text.text = "Blink Step \n" +
+            "To use this technique press the left touchpad \n" +
+            "and you will blink a step in the direction you are looking";
     }
 
     private void SetToArmswingDescription()
     {
-        Text.text = "Armswing \n";
+        Text.text = "Armswing \n" +
+            "To use this technique press the button on the side of the controller \n" +
+            "and swing controller forwards and backwards to move in the direction \n" +
+            "you are looking. This works independently for each controller";
     }
 
     private void SetToCybershoesDescription()
     {
-        Text.text = "Cybershoes \n";
+        Text.text = "Cybershoes \n" +
+            "To use this technique move your feet forward and backward like normal walking";
 
     }
 
     private void SetToSmoothLocomotionDescription()
     {
-        Text.text = "Smooth locomotion \n";
+        Text.text = "Smooth locomotion \n" +
+            "To use this technique hold the left touchpad \n" +
+            "and you will walk in the direction you are looking, \n" + 
+            "this can be used to go backwards too if the touchpad is pressed un the lower half";
     }
 
 }
